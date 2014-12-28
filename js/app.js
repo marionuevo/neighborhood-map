c29409237bead18b68e0dfcf3101a6953cda29fb
Mario GonzalezNuevo Artime <marionuevo@Marios-MacBook-Air.local>
2014-12-28 14:17:06 +0000

    Converted to KO scheme. Added 2 example points at Adeje

--- js/app.js
+++ js/app.js
@@ -1,9 +1,25 @@
-var map;
-function initialize() {
- 	var mapOptions = {
-		zoom: 15,
-		center: new google.maps.LatLng(28.1217968, -16.7339215)
- 	};
-  	map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-}
-google.maps.event.addDomListener(window, 'load', initialize);
+ko.bindingHandlers.googlemap = {
+    init: function (element, valueAccessor) {
+        var value = valueAccessor();
+        var mapOptions = {
+			zoom: 15,
+            center: new google.maps.LatLng(value.centerLat, value.centerLon),
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        var map = new google.maps.Map(element, mapOptions);
+          
+        for (var i in value.locations()) {
+            var latLng = new google.maps.LatLng (value.locations()[i].latitude, value.locations()[i].longitude);
+            var marker = new google.maps.Marker ({position: latLng, map: map});
+        }
+    }
+};
+
+var viewModel =  {
+    locations: ko.observableArray([
+        {name: "Ayuntamiento de Adeje", latitude: 28.1217968, longitude: -16.7339215},
+        {name: "CDTCA", latitude: 28.11559, longitude: -16.730122}
+    ])
+};
+    
+ko.applyBindings(viewModel);
