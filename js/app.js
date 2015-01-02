21f3bfea18e3d3c5614fc321eee20f9f134704f4
Mario Nuevo <marionuevo@Marios-MacPro.local>
2015-01-02 13:36:02 +0000

    Changed locations varname to markers

--- js/app.js
+++ js/app.js
@@ -10,20 +10,20 @@ function viewModel () {
         };
         map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
           
-        for (var i in locations()) {
-            var latLng = new google.maps.LatLng (locations()[i].latitude, locations()[i].longitude);
+        for (var i in markers()) {
+            var latLng = new google.maps.LatLng (markers()[i].latitude, markers()[i].longitude);
             var marker = new google.maps.Marker ({position: latLng, map: map});
         }
     }
 
-    locations = ko.observableArray([
+    markers = ko.observableArray([
         {name: "Ayuntamiento de Adeje", latitude: 28.1217968, longitude: -16.7339215},
         {name: "CDTCA", latitude: 28.11559, longitude: -16.730122},
     ]);
 
     mapInit ();
 
-    // search box
+    // search box provided by Google API
     var input = document.getElementById('input');
     map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
     var searchBox = new google.maps.places.SearchBox(input);
