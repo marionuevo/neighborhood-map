21232cc250a40cffac7147361e3392171f93089f
Mario <marionuevo@gmail.com>
2015-02-11 13:04:42 +0000

    Actions required by evaluator were done.

--- README.md
+++ README.md
@@ -1,11 +1,20 @@
 neighborhood-map project
 ========================
 
-This SPA starts with a map contaning more popular bussines in Adeje, Canary Islands, Spain.
+This SPA starts with a map contaning the more popular bussines in Adeje, Canary Islands, Spain.
+
+To launch the application, just open index.html in your preferred browser.
+
+At the begining, the application launch a default search with any type of business located in Adeje.
+
 Then, you might search for additional or special type of bussines, eg: restaurants or bars...
-using the SearchBox.
+using the SearchBox on the top left. eg: type "restaurants" and press Enter or click on one
+of the autocomplete search suggestions.
+
 On the left places list, you can click to display details and a Streetview thumbnail of 
 that place. You can click on the place directly on the map, as well.
+
+Enjoy!
 
 Resources used:
 https://developers.google.com/maps/documentation/javascript/reference?hl=es
--- js/app.js
+++ js/app.js
@@ -5,6 +5,10 @@
   * @returns nothing.
   */
 function viewModel() {
+	if (typeof google === "undefined") {
+	   $('#alert-modal').modal();
+	   return;
+	}
 	var adeje = new google.maps.LatLng(28.1217968, -16.7339215); // initial location
 	var self = this;
 	var map;
@@ -22,12 +26,16 @@ function viewModel() {
 	locations = ko.observableArray([]);
 
 	/** Invoke click trigger when user clicks over a location list item.
+	  * Also close the list if the screen is in "mobile" mode.
 	  * 
 	  * @param {Location} location: The location object that we want to show info.
 	  * @returns nothing.
 	  */
 	self.showdata = function (location) {
 		google.maps.event.trigger(location.marker, 'click');
+		if ($(".navbar").css('display') === 'block') {
+			$("#menu-button").trigger('click');
+		}	
 	}
 
 	/** Reset active locations on the list to inactive state.
@@ -131,7 +139,7 @@ function viewModel() {
 			var request = {
 				placeId: place.place_id
 			};
-			service.getDetails(request, detailsCallback);
+			service.getDetails(request, callBack_createInfoWindow);
 			infoWindow.open(map, this);
 			resetActiveLocations();
 			location.active(true);
@@ -144,32 +152,35 @@ function viewModel() {
 
 	  * @returns nothing.
 	  */
-	function detailsCallback(place, status) {
+	function callBack_createInfoWindow(place, status) {
 		// error check
-	    if (status == google.maps.places.PlacesServiceStatus.OK) {
-	    	var streetviewURL = 'https://maps.googleapis.com/maps/api/streetview?size=128x128&location=' + place.geometry.location;
-	    	var contentString = '<div class="media"><div class="pull-left" href="#"><img class="media-object img-thumbnail" src="' 
-	    							+ streetviewURL + '" alt="streetview image"></div><div class="media-body"><h4 class="media-heading">' 
-	    							+ place.name + '</h4>' + place.vicinity + '<br>';
+		if (status == google.maps.places.PlacesServiceStatus.OK) {
+			var streetviewURL = 'https://maps.googleapis.com/maps/api/streetview?size=128x128&location=' + place.geometry.location;
+			var contentString = '<div class="media"><div class="pull-left" href="#"><img class="media-object img-thumbnail" src="' 
+									+ streetviewURL + '" alt="streetview image"></div><div class="media-body"><h4 class="media-heading">' 
+									+ place.name + '</h4>' + place.vicinity + '<br>';
 
-	    	if (place.formatted_phone_number) contentString += place.formatted_phone_number +'<br>';
-	    	if (place.url) contentString += '<a href="' + place.url + '">Google+</a>';
-	    	contentString += '</div></div>';
+			if (place.formatted_phone_number) contentString += place.formatted_phone_number +'<br>';
+			if (place.url) contentString += '<a href="' + place.url + '">Google+</a>';
+			contentString += '</div></div>';
 
 			infoWindow.setContent(contentString);
-	    }
+		}
 	};
 
 	mapInit ();
 
 	// search box provided by Google API
+	// the following line selects the htmlinput with "input" id element form the DOM using jQuery
 	var input = $('#input')[0];
+	
 	map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 	var searchBox = new google.maps.places.SearchBox(input);
 
 	// Listen for the event fired when the user selects an item from the search box
 	// pick list. Retrieve the matching places for that item.
 	google.maps.event.addListener(searchBox, 'places_changed', searchBoxCallback);
+	searchBox.setBounds(map.getBounds());
 
 	/** Function called when searchbox results are given.
 	  * Updates markers and locations array with new results
